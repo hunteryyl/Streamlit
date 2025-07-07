b4e9caa50ebd61df46d0a98f97607273a2ba031e
hunteryyl <hunter_yyl@yahoo.com.tw>
2025-07-07 15:06:49 +0800

    Add files via upload

---
+++ Day07.py
@@ -0,0 +1,7 @@
+### Deploy a Streamlit app
+### 
+### To deploy a Streamlit app, do the following:
+### 
+###     Sign in with GitHub or Gmail credentials
+###     Pick a repo, branch and file
+###     Click Deploy
